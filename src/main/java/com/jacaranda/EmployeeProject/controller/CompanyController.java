88677f8ee2cf88bdb0f79267cc5ca91ce96d621a
Fernando Zamora <fernandozamorarico@gmail.com>
2023-12-19 08:13:49 +0100

    Subida

---
+++ src/main/java/com/jacaranda/EmployeeProject/controller/CompanyController.java
@@ -0,0 +1,66 @@
+package com.jacaranda.EmployeeProject.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jacaranda.EmployeeProject.model.Company;
+import com.jacaranda.EmployeeProject.service.CompanyService;
+
+@Controller
+public class CompanyController {
+
+	@Autowired
+	private CompanyService companyService;
+	
+	
+	@GetMapping("/listCompanies")
+	public String listCompanies(Model model) {
+		List<Company> listCompanies = companyService.getCompanies();
+		model.addAttribute("listCompanies", listCompanies);
+		return "listCompanies";
+	}
+	
+	@GetMapping("/addCompany")
+	public String addCompany(Model model) {
+		Company c = new Company();
+		model.addAttribute("c", c);
+		return "addCompany";
+	}
+	
+	@GetMapping("/addCompanyDone")
+	public String addCompanyDone(Model model, Company c) {
+		companyService.addCompany(c);
+		Company company = new Company();
+		model.addAttribute("c", company);
+		String result = "La compañía ha sido añadida con éxito";
+		model.addAttribute("result", result);
+		return "addCompany";
+	}
+	
+	@GetMapping("/editCompany")
+	public String editCompany(@RequestParam Integer id, Model model) {
+		Optional<Company> company = companyService.getById(id);
+		if (company != null) {
+			model.addAttribute("c", company);
+		}
+		return "editCompany";
+	}
+	
+	@GetMapping("/editCompanyDone")
+	public String editCompanyDone(@ModelAttribute("c") Company cUpdate, Model model) {
+		Company comp = new Company();
+		
+		model.addAttribute("c", comp);
+		companyService.editCompany(cUpdate);
+		String result = "La compañía ha sido editada con éxito";
+		model.addAttribute("result", result);
+		return "editCompany";
+	}
+}
---
+++ src/main/java/com/jacaranda/EmployeeProject/service/CompanyService.java
@@ -0,0 +1,38 @@
+package com.jacaranda.EmployeeProject.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jacaranda.EmployeeProject.model.Company;
+import com.jacaranda.EmployeeProject.repository.CompanyRepository;
+
+@Service
+public class CompanyService {
+
+	@Autowired
+	private CompanyRepository companyRepository;
+	
+	public List<Company> getCompanies(){
+		return companyRepository.findAll();
+	}
+	
+	public Optional<Company> getById(Integer id) {
+		return companyRepository.findById(id);
+	}
+	
+	public Company addCompany(Company c) {
+		return companyRepository.save(c);
+	}
+	
+	public Company editCompany(Company c) {
+		Company comp = getById(c.getId()).orElse(null);
+		comp.setName(c.getName());
+		comp.setAddress(c.getAddress());
+		comp.setCity(c.getCity());
+		comp = companyRepository.save(comp);
+		return comp;
+	}
+}
